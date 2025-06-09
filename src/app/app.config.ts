df53ffbba1a1c22e6ee0f99e9a201605f88a45c2
Samuel Oliveira da Silva <samosaara@gmail.com>
2025-06-10 00:45:10 +0200

    feat: adds angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,20 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import {
+  getRemoteConfig,
+  provideRemoteConfig,
+} from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +31,23 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'thuune-slva',
+        appId: '1:937151099706:web:cfee98622d9f9cfdb9d603',
+        storageBucket: 'thuune-slva.firebasestorage.app',
+        apiKey: 'AIzaSyAQVogNxvGy4lp0YRbSROeSmQ7HRqufWgI',
+        authDomain: 'thuune-slva.firebaseapp.com',
+        messagingSenderId: '937151099706',
+        measurementId: 'G-8L0KKYT40N',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
   ],
 };
